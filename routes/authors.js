fd4afde10b7bad4e7aec8e9d61dbf186fe3ec7ca
Manuel <manueljuliocasanova@gmail.com>
2023-08-15 04:03:27 -0400

    Initial Author Routes

---
+++ routes/authors.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router();
+const Author = require("../models/author");
+//All Authors Route
+
+router.get("/", async (req, res) => {
+  let searchOptions = {};
+  if (req.query.name != null && req.query.name !== "") {
+    // Yes, you're correct! In the context of regular expressions, the "i" flag stands for
+    // "case-insensitive." When you use the "i" flag in a regular expression, it indicates that the pattern
+    // matching should be done without regard to the case of the letters. This means that it will match both
+    // uppercase and lowercase letters.
+
+    searchOptions.name = new RegExp(req.query.name, "i");
+  }
+
+  try {
+    const authors = await Author.find(searchOptions);
+    res.render("authors/index", { authors: authors, searchOptions: req.query });
+    console.log("here");
+  } catch {
+    res.redirect("/");
+  }
+});
+
+//New Author Route
+router.get("/new", (req, res) => {
+  res.render("authors/new", { author: new Author() });
+});
+
+// Create Author Route
+router.post("/", async (req, res) => {
+  const author = new Author({
+    name: req.body.name,
+  });
+  try {
+    const newAuthor = await author.save();
+    //     // res.redirect(`authors/${newAuthor.id}`);
+    res.redirect(`authors`);
+  } catch {
+    res.render("authors/new", {
+      author: author,
+      errorMessage: "Error creating Author",
+    });
+  }
+});
+
+module.exports = router;
